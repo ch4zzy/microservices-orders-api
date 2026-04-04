a0423596b8f2a07ba3ee2d905943a27555623ff8
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2026-04-04 11:20:23 +0300

    fix

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ app.include_router(router)
 instrumentator = Instrumentator(
     should_group_status_codes=False,
     should_ignore_untemplated=True,
-    should_respect_env_var=True,
+    should_respect_env_var=False,
     should_instrument_requests_inprogress=True,
     excluded_handlers=["/metrics"],
     env_var_name="ENABLE_METRICS",
